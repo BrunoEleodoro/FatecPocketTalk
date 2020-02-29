34357f1b0b20816f21e42bb14e5ecf0a61ea2bc6
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2020-02-29 08:48:42 -0300

    Updates

---
+++ precache-manifest.7bee0265d45f8f59bd84bbc3db88835c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ae28206f15b099e47d8f779280ada0",
+    "url": "/FatecPocketTalk/index.html"
+  },
+  {
+    "revision": "ba38f7c6f42e735b1c1b",
+    "url": "/FatecPocketTalk/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "01df11ca77889666439c",
+    "url": "/FatecPocketTalk/static/js/2.f3c3d949.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/FatecPocketTalk/static/js/2.f3c3d949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba38f7c6f42e735b1c1b",
+    "url": "/FatecPocketTalk/static/js/main.f7d388c0.chunk.js"
+  },
+  {
+    "revision": "d35d0b460c9c4ca6cdfc",
+    "url": "/FatecPocketTalk/static/js/runtime-main.c7cf72db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FatecPocketTalk/static/media/logo.5d5d9eef.svg"
+  }
+]);
